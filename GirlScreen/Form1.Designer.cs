739bc592ec6ccd370322a24fdcedb16dc095dcf4
Toby Skarting <toby33022002@gmail.com>
2015-11-20 00:31:27 +0800

    First commit

---
+++ GirlScreen/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace GirlScreen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuItemFullScreen = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemChangePicture = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemSetPassword = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemLockScreen = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.labelStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.pictureBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::GirlScreen.Properties.Resources.tumblr_nxe3lt4ywj1ukq93to1_1280;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(284, 262);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuItemFullScreen,
+            this.menuItemChangePicture,
+            this.menuItemSetPassword,
+            this.menuItemLockScreen,
+            this.menuItemExit});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(177, 114);
+            // 
+            // menuItemFullScreen
+            // 
+            this.menuItemFullScreen.Name = "menuItemFullScreen";
+            this.menuItemFullScreen.Size = new System.Drawing.Size(176, 22);
+            this.menuItemFullScreen.Text = "全螢幕 (Alt+Enter)";
+            this.menuItemFullScreen.Click += new System.EventHandler(this.menuItemFullScreen_Click);
+            // 
+            // menuItemChangePicture
+            // 
+            this.menuItemChangePicture.Name = "menuItemChangePicture";
+            this.menuItemChangePicture.Size = new System.Drawing.Size(176, 22);
+            this.menuItemChangePicture.Text = "更換圖片";
+            this.menuItemChangePicture.Click += new System.EventHandler(this.menuItemChangePicture_Click);
+            // 
+            // menuItemSetPassword
+            // 
+            this.menuItemSetPassword.Name = "menuItemSetPassword";
+            this.menuItemSetPassword.Size = new System.Drawing.Size(176, 22);
+            this.menuItemSetPassword.Text = "設定密碼";
+            this.menuItemSetPassword.Click += new System.EventHandler(this.menuItemSetPassword_Click);
+            // 
+            // menuItemLockScreen
+            // 
+            this.menuItemLockScreen.Name = "menuItemLockScreen";
+            this.menuItemLockScreen.Size = new System.Drawing.Size(176, 22);
+            this.menuItemLockScreen.Text = "鎖定電腦";
+            this.menuItemLockScreen.Click += new System.EventHandler(this.menuItemLockScreen_Click);
+            // 
+            // menuItemExit
+            // 
+            this.menuItemExit.Name = "menuItemExit";
+            this.menuItemExit.Size = new System.Drawing.Size(176, 22);
+            this.menuItemExit.Text = "結束";
+            this.menuItemExit.Click += new System.EventHandler(this.menuItemExit_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "圖片檔案|*.png;*.jpg;*.gif;*.bmp;*.jpeg";
+            this.openFileDialog1.Title = "選擇圖片";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(0, 12);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '●';
+            this.textBoxPassword.Size = new System.Drawing.Size(284, 22);
+            this.textBoxPassword.TabIndex = 2;
+            this.textBoxPassword.Visible = false;
+            this.textBoxPassword.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(-2, 0);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(51, 12);
+            this.labelStatus.TabIndex = 3;
+            this.labelStatus.Text = "Password:";
+            this.labelStatus.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "GirlScreen";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuItemFullScreen;
+        private System.Windows.Forms.ToolStripMenuItem menuItemChangePicture;
+        private System.Windows.Forms.ToolStripMenuItem menuItemExit;
+        private System.Windows.Forms.ToolStripMenuItem menuItemLockScreen;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.ToolStripMenuItem menuItemSetPassword;
+    }
+}
+
---
+++ GirlScreen/Form1.cs
@@ -0,0 +1,267 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace GirlScreen
+{
+    public partial class Form1 : Form
+    {
+        private byte[] password;
+
+        //Declare MouseHookProcedure as a HookProc type.
+        WinAPI.HookProc MouseHookProcedure;
+        WinAPI.HookProc KeyboardHookProcedure;
+        private static int hMouseHook, hKeyboardHook;
+
+        public Form1()
+        {
+            RegistryKey reg = Registry.CurrentUser.CreateSubKey("Software\\GirlScreen");
+            password = (byte[])reg.GetValue("test", new byte[] {0xd4, 0x1d, 0x8c, 0xd9, 0x8f, 0x00, 0xb2, 0x04, 0xe9, 0x80, 0x09, 0x98, 0xec, 0xf8, 0x42, 0x7e});
+            InitializeComponent();
+        }
+        
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (contextMenuStrip1.Enabled == false)
+                e.Cancel = true;
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            if (contextMenuStrip1.Enabled == false)
+                LockMouse();
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            if (contextMenuStrip1.Enabled == false)
+                LockMouse();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Alt == true && e.KeyCode == Keys.Enter)
+                ToogleFullScreen();
+        }
+
+        private void menuItemFullScreen_Click(object sender, EventArgs e)
+        {
+            ToogleFullScreen();
+        }
+
+        private void menuItemChangePicture_Click(object sender, EventArgs e)
+        {
+            LoadPicture();
+        }
+
+        private void menuItemSetPassword_Click(object sender, EventArgs e)
+        {
+            labelStatus.Visible = true;
+            textBoxPassword.Visible = true;
+        }
+
+        private void menuItemLockScreen_Click(object sender, EventArgs e)
+        {
+            menuItemLockScreen.Checked = !menuItemLockScreen.Checked;
+            if (menuItemLockScreen.Checked)
+                LockComputer();
+            else
+                UnLockComputer();
+        }
+
+        private void menuItemExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void pictureBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (contextMenuStrip1.Enabled)
+                LoadPicture();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Return)
+            {
+                MD5 md5 = MD5.Create();
+                md5.ComputeHash(Encoding.UTF8.GetBytes(textBoxPassword.Text));
+
+                if (contextMenuStrip1.Enabled)
+                {
+                    labelStatus.Visible = false;
+                    textBoxPassword.Visible = false;
+                    password = md5.Hash;
+                    RegistryKey reg = Registry.CurrentUser.CreateSubKey("Software\\GirlScreen");
+                    reg.SetValue("test", md5.Hash, RegistryValueKind.Binary);
+                }
+                else if (System.Collections.StructuralComparisons.StructuralEqualityComparer.Equals(md5.Hash, password))
+                {
+                    textBoxPassword.Clear();
+                    UnLockComputer();
+                    menuItemLockScreen.Checked = false;
+                }
+            }
+        }
+        
+        private void ToogleFullScreen()
+        {
+            menuItemFullScreen.Checked = !menuItemFullScreen.Checked;
+            if (menuItemFullScreen.Checked)
+            {
+                this.FormBorderStyle = FormBorderStyle.None;
+                this.WindowState = FormWindowState.Maximized;
+            }
+            else
+            {
+                this.FormBorderStyle = FormBorderStyle.SizableToolWindow;
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void LoadPicture()
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+                this.ClientSize = pictureBox1.Image.Size;
+            }
+        }
+
+        private void DisablePolicies(bool disabled)
+        {
+            RegistryKey reg;
+            reg = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+            reg.SetValue("DisableTaskMgr", disabled, RegistryValueKind.DWord);
+            reg.SetValue("DisableLockWorkstation", disabled, RegistryValueKind.DWord);
+            reg.SetValue("DisableChangePassword", disabled, RegistryValueKind.DWord);
+            reg = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer");
+            reg.SetValue("NoLogoff", disabled, RegistryValueKind.DWord);
+            reg.SetValue("NoClose", disabled, RegistryValueKind.DWord);
+            reg = Registry.LocalMachine.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+            reg.SetValue("HideFastUserSwitching", disabled, RegistryValueKind.DWord);
+        }
+      
+        private void LockMouse()
+        {
+            Rectangle controlRectangle = textBoxPassword.RectangleToScreen(textBoxPassword.ClientRectangle);
+            WinAPI.RECT rect;
+            rect.Left = controlRectangle.Left;
+            rect.Top = controlRectangle.Top;
+            rect.Right = controlRectangle.Right;
+            rect.Bottom = controlRectangle.Bottom;
+            WinAPI.ClipCursor(ref rect);
+            WinAPI.SetActiveWindow(Handle);
+            WinAPI.SetForegroundWindow(this.Handle);
+        }
+
+        public static int KeyboardHookProc(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode < 0)
+            {
+                return WinAPI.CallNextHookEx(hKeyboardHook, nCode, wParam, lParam);
+            }
+            else
+            {
+                switch ((int)wParam)
+                {
+
+                    case WinAPI.WM_KEYDOWN:
+                    case WinAPI.WM_KEYUP:
+                        switch (Marshal.ReadInt32(lParam))
+                        {
+                            case (int)WinAPI.VK.LWIN:
+                            case (int)WinAPI.VK.RWIN:
+                            case (int)WinAPI.VK.ESCAPE:
+                            case (int)WinAPI.VK.TAB:
+                                return 1;
+                            default:
+                                break;
+                        }
+                        break;
+                    case 0x104:
+                    case 0x105:
+                    case 0x106:
+                    case 0x107:
+                        return 1;
+                    default:
+                        break;
+                }
+            }
+            return WinAPI.CallNextHookEx(hKeyboardHook, nCode, wParam, lParam);
+        }
+
+        private void ToogleHook()
+        {
+            if (hKeyboardHook == 0)
+            {
+                // Create an instance of HookProc.
+                KeyboardHookProcedure = new WinAPI.HookProc(KeyboardHookProc);
+
+                hKeyboardHook = WinAPI.SetWindowsHookEx(WinAPI.WH_KEYBOARD_LL,
+                            KeyboardHookProcedure,
+                            (IntPtr)0,
+                            0);
+                if (hKeyboardHook == 0)
+                {
+                    MessageBox.Show("SetWindowsHookEx Failed");
+                    return;
+                }
+            }
+            else
+            {
+                bool ret = WinAPI.UnhookWindowsHookEx(hKeyboardHook);
+                if (ret == false)
+                {
+                    MessageBox.Show("UnhookWindowsHookEx Failed");
+                    return;
+                }
+                hKeyboardHook = 0;
+            }
+        }
+
+        private void LockComputer()
+        {
+            Process[] p = Process.GetProcesses();
+            foreach (Process p1 in p)
+            {
+                try
+                {
+                    if (p1.ProcessName.ToLower().Trim() == "taskmgr")
+                    {
+                        p1.Kill();
+                    }
+                }
+                catch
+                {
+                }
+            }
+
+            LockMouse();
+            ToogleHook();
+            DisablePolicies(true);
+            contextMenuStrip1.Enabled = false;
+            labelStatus.Visible = true;
+            textBoxPassword.Visible = true;
+        }
+
+        private void UnLockComputer()
+        {
+            WinAPI.ClipCursor(IntPtr.Zero);
+            DisablePolicies(false);
+            contextMenuStrip1.Enabled = true;
+            labelStatus.Visible = false;
+            textBoxPassword.Visible = false;
+        }
+        
+    }
+}
